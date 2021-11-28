2fc37305e1d390b81d16edae1f784f046f7ecc74
b4kii <dami4nhalibart@gmail.com>
2021-11-28 21:48:21 +0100

    project.sh

--- scripts/project.sh
+++ scripts/project.sh
@@ -9,8 +9,3 @@ target_compile_options()' > CMakeLists.txt
 mkdir ./build
 mkdir ./src
 echo "Project '"$1"' has been created"
-mkdir $1
-mkdir ./$1/build
-mkdir ./$1/src
-touch ./$1/CMakeLists.txt
-echo "Project $1 has been created"
