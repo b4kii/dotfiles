a2db5cd05fd14fdc1fc51c9880013dae00f51607
b4kii <dami4nhalibart@gmail.com>
2021-11-28 21:46:07 +0100

    Project.sh

--- scripts/project.sh
+++ scripts/project.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#!/bin/bash
+mkdir $1
+cd $1
+printf 'cmake_minimum_required()
+project()
+add_executable()
+target_compile_features()
+target_compile_options()' > CMakeLists.txt
+mkdir ./build
+mkdir ./src
+echo "Project '"$1"' has been created"
 mkdir $1
 mkdir ./$1/build
 mkdir ./$1/src
