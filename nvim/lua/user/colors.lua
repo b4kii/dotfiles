aff2a6c5a5bccb3f43769ccf321388d521beff1e
b4kii <dami4nhalibart@gmail.com>
2022-04-15 00:09:23 +0200

    Pcall colorscheme

--- nvim/lua/user/colors.lua
+++ nvim/lua/user/colors.lua
@@ -1,1 +1,8 @@
-vim.cmd("colorscheme ayu-mirage")
+local colorscheme = "ayu-mirage"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not status_ok then
+    vim.notify("colorscheme " .. colorscheme .. "not found!")
+end
+-- vim.cmd("colorscheme ayu-mirage")
