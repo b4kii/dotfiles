f3e8f4e43062190ac884279d1d3d43860aeefa3b
b4kii <dami4nhalibart@gmail.com>
2022-04-19 20:44:05 +0200

    Wasting time again

--- windows-powershell/powershell/user_profile.ps1
+++ windows-powershell/powershell/user_profile.ps1
@@ -1,12 +1,22 @@
 # Prompt
-
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt powerlevel10k_rainbow
 
+Set-PoshPrompt powerlevel10k_lean
+
+# Icons
+Import-Module Terminal-Icons
+
+# AutoCompletion
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadLineChordProvider 'Ctrl+f' -PSReadLineChordReverseHistory 'Ctrl+r'
 
 # Aliases
-
 Set-Alias vi nvim
 Set-Alias ll ls
 Set-Alias g git
