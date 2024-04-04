75239d3373bb06c07fc50676e4235b50250b2eb2
b4kii <dami4nhalibart@gmail.com>
2024-04-04 19:36:32 +0200

    xd

--- windows-powershell/powershell/user_profile.ps1
+++ windows-powershell/powershell/user_profile.ps1
@@ -25,10 +25,12 @@ Set-Alias lg lazygit
 function sync-data {
     $homeDir = $env:USERPROFILE
 
+    $recentBackupFile = Get-Childitem -Path C:\Users\baki\Documents\PowerToys\Backup\ | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+
     $paths = @(
         "$homeDir\AppData\Roaming\Code\User\settings.json",
         "$homeDir\AppData\Roaming\Code\User\keybindings.json",
-        "$homeDir\Documents\PowerToys\Backup\*.ptb"
+        $recentBackupFile
     )
 
     $destinations = @(
@@ -40,7 +42,6 @@ function sync-data {
     for ($i = 0; $i -lt $paths.Length; $i++) {
         Copy-Item -Path $paths[$i] -Destination $destinations[$i] -Recurse
     }
-
 }
 
 function ex {
