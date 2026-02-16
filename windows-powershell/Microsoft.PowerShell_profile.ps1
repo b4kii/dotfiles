827a7aac2f0e4588d8457ece08c93c1f3528e5be
b4kii <45925135+b4kii@users.noreply.github.com>
2026-02-16 07:58:09 +0100

    Update Microsoft.PowerShell_profile.ps1

--- windows-powershell/Microsoft.PowerShell_profile.ps1
+++ windows-powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,11 @@
 $PSStyle.FileInfo.Directory = ""
 
+# AutoCompletion
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 
+# Fzf
 Import-Module PSFzf
 Set-PsFzfOption -PSReadLineChordProvider 'Alt+f' -PSReadLineChordReverseHistory 'Alt+r'
 
@@ -15,3 +17,5 @@ Set-Alias vi nvim
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias lg lazygit
+
+Invoke-Expression (&starship init powershell)
