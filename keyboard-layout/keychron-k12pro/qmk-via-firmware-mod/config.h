64b493a6c8d2025f5022068104195c13f5f0dddb
b4kii <dami4nhalibart@gmail.com>
2026-06-23 23:17:34 +0200

    k12-pro new firmware options

--- keyboard-layout/keychron-k12pro/qmk-via-firmware-mod/config.h
+++ keyboard-layout/keychron-k12pro/qmk-via-firmware-mod/config.h
@@ -6,14 +6,21 @@
 #define MK_KINETIC_SPEED
 
 // Domyślne wartości Kinetic Mode
-#define MOUSEKEY_DELAY 5
+//#define MOUSEKEY_DELAY 5
+//#define MOUSEKEY_DELAY 3
+#define MOUSEKEY_DELAY 2
+
 #define MOUSEKEY_INTERVAL 10
 #define MOUSEKEY_MOVE_DELTA 16
 
-#define MOUSEKEY_INITIAL_SPEED 70
+//#define MOUSEKEY_INITIAL_SPEED 70
+//#define MOUSEKEY_INITIAL_SPEED 150
+//#define MOUSEKEY_INITIAL_SPEED 450
+#define MOUSEKEY_INITIAL_SPEED 350
+
 #define MOUSEKEY_BASE_SPEED 4500
 #define MOUSEKEY_DECELERATED_SPEED 250
-#define MOUSEKEY_ACCELERATED_SPEED 3500
+#define MOUSEKEY_ACCELERATED_SPEED 5500
 
 // Domyślne wartości Kinetic Mode dla scrolla
 #define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 16
@@ -21,28 +28,3 @@
 #define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 48
 #define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 8
 
-// #pragma once
-
-// #define DYNAMIC_KEYMAP_LAYER_COUNT 5
-
-// // ===== ACCELERATED MODE =====
-// // Bez #define MK_KINETIC_SPEED
-
-// // Ruch kursora
-// #define MOUSEKEY_DELAY 10
-// #define MOUSEKEY_INTERVAL 20
-
-// // Mały pierwszy krok — dzięki temu MS_ACL0 będzie precyzyjny i wolny,
-// // zbliżony do początku ruchu w Twoim obecnym kinetic.
-// #define MOUSEKEY_MOVE_DELTA 2
-
-// // Normalny ruch bez MS_ACLx: standardowy QMK max speed.
-// #define MOUSEKEY_MAX_SPEED 10
-// #define MOUSEKEY_TIME_TO_MAX 30
-
-// // Scroll — domyślne wartości QMK
-// #define MOUSEKEY_WHEEL_DELAY 10
-// #define MOUSEKEY_WHEEL_INTERVAL 80
-// #define MOUSEKEY_WHEEL_DELTA 1
-// #define MOUSEKEY_WHEEL_MAX_SPEED 8
-// #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
